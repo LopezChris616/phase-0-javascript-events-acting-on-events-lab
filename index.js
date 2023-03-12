6861ae48ce1ce233a85b30c2d510351ed845bc55
Christian Lopez <lopezchristian1610@gmail.com>
2023-03-12 01:14:21 -0500

    dodger able to moved to the left and right

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+const leftNumbers = dodger.style.left.replace("px", "");
+let left = parseInt(leftNumbers, 10);
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerLeft() {
+    if (left > 0) {
+        left -= 1;
+        dodger.style.left = `${left}px`;
+    }
+}
+
+function moveDodgerRight() {
+    left += 1;
+    if(left <= 360) {
+        dodger.style.left = `${left}px`;
+    }
+}
